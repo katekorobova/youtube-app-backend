980860ba63384ccd9e11e25960951aa9e792a36f
Ekaterina Korobova <katekorobova@yandex.ru>
2025-11-10 14:06:27 +1100

    OPTIONS method is now allowed

--- src/main/java/com/verycoolprojects/youtubeapp/config/SecurityConfig.java
+++ src/main/java/com/verycoolprojects/youtubeapp/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true); // Allow credentials
         configuration.setAllowedOrigins(allowedOrigins);
-        configuration.setAllowedMethods(List.of("GET", "POST"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
